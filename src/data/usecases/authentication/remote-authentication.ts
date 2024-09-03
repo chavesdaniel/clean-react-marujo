3c6926667c094bcfa9e227f7b7031995fb3624d8
Daniel Chaves <chavesdaniel@gmail.com>
2024-09-03 19:16:46 -0300

    feat: ensure RemoteAuthentication throws InvalidCredentialsError on 401

--- src/data/usecases/authentication/remote-authentication.ts
+++ src/data/usecases/authentication/remote-authentication.ts
@@ -1,5 +1,7 @@
 import { AuthenticationParams } from "@/domain/usecases/authentication"
 import { HttpPostClient } from "@/data/protocols/http/http-post-client"
+import { HttpStatusCode } from "@/data/protocols/http/http-response"
+import { InvalidCredentialsError } from "@/domain/errors/invalid-credentials-error"
 
 export class RemoteAuthentication {
   constructor(
@@ -7,9 +9,13 @@ export class RemoteAuthentication {
     private readonly httpPostClient: HttpPostClient
   ) { }
   async auth(params: AuthenticationParams): Promise<void> {
-    await this.httpPostClient.post({
+    const httpResponse = await this.httpPostClient.post({
       url: this.url,
       body: params
     })
+    switch (httpResponse.statusCode) {
+      case HttpStatusCode.unathorized: throw new InvalidCredentialsError
+      default: return Promise.resolve()
+    }
   }
 }
